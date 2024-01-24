204740c8d5ceeeab756d676499938068c8881540
Krasyo <doanusho@gmail.com>
2024-01-24 22:21:50 +0000

    Add rest of the db tables

---
+++ construction/Models/AdminModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace construction.Models;
+
+[Table("admin")]
+public class Admin
+{
+    [Column("id")]
+    public int Id { get; set; }
+    [Column("name")]
+    public string Name { get; set; }
+    [Column("email")]
+    public string Email { get; set; }
+    [Column("password")]
+    public string Password { get; set; }
+    [Column("role")]
+    public string Role { get; set; }
+}
---
+++ construction/Models/BussinessInfoModel.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace construction.Models;
+
+[Table("bussiness_info")]
+public class BussinessInfo
+{
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    public string Name { get; set; }
+
+    [Column("email")]
+    public string? Email { get; set; }
+
+    [Column("phone")]
+    public string? Phone { get; set; }
+
+    [Column("address")]
+    public string? Address { get; set; }
+
+    [Column("city")]
+    public string? City { get; set; }
+
+    [Column("info")]
+    public string? Info { get; set; }
+
+    [Column("logo")]
+    public string? Logo { get; set; }
+
+    [Column("facebook")]
+    public string? Facebook { get; set; }
+
+    [Column("instagram")]
+    public string? Instagram { get; set; }
+
+    [Column("youtube")]
+    public string? Youtube { get; set; }
+
+    [Column("tiktok")]
+    public string? Tiktok { get; set; }
+
+    [Column("linkedin")]
+    public string? Linkedin { get; set; }
+}
