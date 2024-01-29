dfe175e1ceb1444cb1312d27fcc729db9cccc3c3
Krasyo <doanusho@gmail.com>
2024-01-29 22:12:44 +0000

    Deployment Set up CD with Azure

---
+++ Scripts/DeployScriptConstructionBackend.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+CONTAINER_NAME="construction-back"
+IMAGE_NAME="krahristov/construction-backend:latest"
+APP_DIR="$HOME/construction-backend"
+
+echo "Starting deployment..."
+
+echo "Pulling the Docker image..."
+sudo docker pull $IMAGE_NAME
+
+echo "Stopping the existing Docker container..."
+sudo docker stop $CONTAINER_NAME || true
+sudo docker rm $CONTAINER_NAME || true
+
+echo "Running the new Docker container..."
+sudo docker run -p 8080:8080 -d --name $CONTAINER_NAME $IMAGE_NAME
+
+echo "Removing unused Docker images..."
+sudo docker image prune -af
+
+echo "Deployment successful!"
