108b5f37917a4e8cce31978963d3ee6d8269d256
Krasyo <doanusho@gmail.com>
2024-02-01 19:59:15 +0000

    Get Job Types Add get job types dto add job types data

---
+++ construction/Dtos/JobTypesDtos.cs
@@ -0,0 +1,9 @@
+namespace construction.Dtos;
+
+public class GetJobTypesDto
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public string Icon { get; set; }
+}
