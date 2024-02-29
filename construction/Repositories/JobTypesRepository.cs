72c737396919a83b783a0928fd99ffa48b3f21e4
Krasyo <doanusho@gmail.com>
2024-02-29 17:57:19 +0000

    Add count of jobs to job types on get all job types endpoint. Add order by name to sql query

--- construction/Repositories/JobTypesRepository.cs
+++ construction/Repositories/JobTypesRepository.cs
@@ -52,7 +52,8 @@ public class JobTypesRepository : IJobTypesRepository
         StringBuilder sql = new StringBuilder("SELECT job_types.name, job_types.description, job_types.image, job_types.icon, COUNT(jobs.job_id) ");
         sql.Append("FROM job_types ");
         sql.Append("LEFT JOIN jobs ON job_types.name = jobs.job_type ");
-        sql.Append("GROUP BY job_types.name");
+        sql.Append("GROUP BY job_types.name ");
+        sql.Append("ORDER BY job_types.name");
 
         return await connection.QueryAsync<GetJobTypeDto>(sql.ToString());
     }
